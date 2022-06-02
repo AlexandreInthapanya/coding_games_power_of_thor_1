169880ff7a1dc72891b190b493c42ff7bcdb74aa
Alex <anou_web@hotmail.com>
2022-06-02 13:20:36 +0200

    add commentary

--- powerOfThor1.js
+++ powerOfThor1.js
@@ -17,25 +17,25 @@
  while (true) {
      const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
  
-     var direction = '';
+     var direction = ''; //initialisation de la variable direction 
  
-     if (thorY > lightY && thorY > 0){
-         direction += 'N';
-         thorY--;
-     } else if (thorY < lightY && thorY < 17){
-         direction += 'S';
-         thorY++;
+     if (thorY > lightY && thorY > 0){ //si la position de thor en ordonnée est supérieur à la position de l'éclair en ordonnée et que la position de thor en ordonnée est supérieur à 0 sont vraies 
+         direction += 'N'; //alors ajouter redirection vers Nord
+         thorY--; //décrémentation -1
+     } else if (thorY < lightY && thorY < 17){ //sinon si la position de thor en ordonnée est inférieur à la position de l'éclair en ordonnée et que la position de thor en ordonnée est inférieur à 17 sont vraies 
+         direction += 'S'; //alors ajouter redirection vers Sud
+         thorY++; //incrémentation +1
      }
  
-     if (thorX > lightX && thorX > 0){
-         direction += 'W';
-         thorX--;
-     } else if (thorX < lightX && thorX < 39){
-         direction += 'E';
-         thorX++;
+     if (thorX > lightX && thorX > 0){ //si la position de thor en abscisse est supérieur à la position de l'éclair en abscisse et que la position de thor en ordonnée est supérieur à 0 sont vraies 
+         direction += 'W'; //alors ajouter redirection vers West
+         thorX--; //décrémentation -1
+     } else if (thorX < lightX && thorX < 39){ //sinon si la position de thor en abscisse est inférieur à la position de l'éclair en abscisse et que la position de thor en ordonnée est inférieur à 39 sont vraies 
+         direction += 'E'; //alors ajouter redirection vers Est
+         thorX++; //incrémentation +1
      } 
  
  
      // A single line providing the move to be made: N NE E SE S SW W or NW
-     console.log(direction);
+     console.log(direction); 
  }
